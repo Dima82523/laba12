4bc500a5854f9b43e14f155a9449f4431a51df79
KodokuNoKami <theprokot@gmail.com>
2023-05-13 14:09:49 +0300

    change ChatActivity.kt(add ComposeButtonPreview)

--- app/src/main/java/com/topic3/android/reddit/screens/ChatActivity.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/ChatActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.topic3.android.reddit.databinding.ActivityChatBinding
@@ -63,7 +64,11 @@ private fun ComposeButton(onButtonClick:() -> Unit){
     }
 }
 
-
+@Preview(showBackground = true)
+@Composable
+    private fun ComposeButtonPreview(){
+    ComposeButton{}
+}
 
     private fun showToast() {
         Toast.makeText(this, "Imaginary chat started!", Toast.LENGTH_SHORT).show()
